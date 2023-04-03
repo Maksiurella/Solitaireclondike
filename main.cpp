252fe494a393b07b3c8bee3c7eacc06f8d361c32
Maksiurella <129667944+Maksiurella@users.noreply.github.com>
2023-04-03 18:36:52 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class game {
+
+char hearts = 'h'; //kier
+char spades = 's'; //pik
+char diamond = 'd'; //karo
+char club = 'c'; //trefl
+
+
+
+};
+
+int main()
+{
+    std::cout << "C++";
+}
